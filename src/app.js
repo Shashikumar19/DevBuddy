2e1656578b902e0be25d2c94ade995a16e435cef
shashikumar <rathodshashikumar72@gmail.com>
2026-08-21 17:09:04 +0530

    Error handling using globle middleware

--- src/app.js
+++ src/app.js
@@ -1,65 +1,37 @@
 console.log("starting the application");
 const express = require('express');
-const {userAuth,adminAuth} = require('./middleware/middleware')
+const { userAuth, adminAuth } = require('./middleware/middleware')
 const app = express();
 
 //request handler
 // multiple route handlers
+app.use('/', (error, req, res, next) => {
 
-app.get('/user/login',(req,res)=>{
-    res.send('user_Loged in successfully')
-})
-app.use('/admin',adminAuth);
-// app.use("/user",userAuth);
-
-app.get('/admin/getAlldata',(req,res,next)=>{
-res.send('ALL DATA IS FETCHED')
+    if (error) {
+        res.send('Something went wrong')
+    }
 })
 
-app.get('/admin/deletedData',(req,res,next)=>{
-res.send('ALL DATA IS DELETED')
+app.get('/user', (req, res, next) => {
+
+    // throw Error('Intentailnal error')
+    // res.send('user_Loged in successfully');
+    next();
 })
 
-
-
-app.post('/user/userprofile',userAuth,(req,res)=>{
- res.send('USER DETAILS FETCHED')
+app.get('/user',(req,res)=>{
+    res.send("resolved")
 })
 
-app.get('/user/userdelete',userAuth,(req,res)=>{
- res.send('USER DELETED')
-})
+app.use('/', (error, req, res, next) => {
 
-
-
-
-
-
-app.get("/abc/:gender/:pass",(req,res)=>{
-    const parmas = req.params;
-    const queryParams = req.query;
-    console.log("params-->",parmas);
-    console.log("query params-->",queryParams)
-    res.send({name:'abc'})
-})
-
-app.post('/user',(req,res)=>{
-    res.send('POST API CALL');
-})
-
-app.put('/user',(req,res)=>{
-    res.send('PUT API CALL');
-})
-
-app.patch('/user',(req,res)=>{
-    res.send('PATCH API CALL');
-})
-app.delete('/user',(req,res)=>{
-res.send("DELETE API CALL")
+    if (error) {
+        res.status(500).send('Something went wrong')
+    }
 })
 
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("App running on the 3000 port");
 })
